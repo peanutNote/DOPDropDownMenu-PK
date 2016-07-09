41c9d812f4993394a46851791b8271ff8e69d7a2
yixiong <yixiong@chemanman.com>
2016-07-09 18:28:35 +0800

    Initial Commit
    DOPDropDownMenu-Enhanced 支持三级菜单选择

--- DOPDropDownMenu-PK/ViewController.m
+++ DOPDropDownMenu-PK/ViewController.m
@@ -7,9 +7,22 @@
 //
 
 #import "ViewController.h"
+#import "DOPDropDownMenu.h"
 
-@interface ViewController ()
+@interface ViewController ()<DOPDropDownMenuDataSource,DOPDropDownMenuDelegate>
+@property (nonatomic, strong) NSArray *provinces;
+@property (nonatomic, strong) NSArray *city1;
+@property (nonatomic, strong) NSArray *city2;
+@property (nonatomic, strong) NSArray *address1_1;
+@property (nonatomic, strong) NSArray *address1_2;
+@property (nonatomic, strong) NSArray *address1_3;
+@property (nonatomic, strong) NSArray *classifys;
+@property (nonatomic, strong) NSArray *cates;
+@property (nonatomic, strong) NSArray *movices;
+@property (nonatomic, strong) NSArray *hostels;
 
+@property (nonatomic, strong) NSArray *sorts;
+@property (nonatomic, weak) DOPDropDownMenu *menu;
 @end
 
 @implementation ViewController
@@ -17,6 +30,195 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"DOPDropDownMenu";
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"重新加载" style:UIBarButtonItemStylePlain target:self action:@selector(menuReloadData)];
+    // 数据
+    self.provinces = @[@"湖北省", @"北京",@"上海"];
+    self.city1 = @[@"武汉市", @"宜昌市", @"孝感市"];
+    self.city2 = @[@"北京市"];
+    self.address1_1 = @[@"江岸区", @"江汉区", @"武昌区"];
+    self.address1_2 = @[@"西陵区", @"宜都区"];
+    self.address1_3 = @[@"孝南区"];
+    self.classifys = @[@"美食",@"今日新单",@"电影",@"酒店"];
+    self.cates = @[@"自助餐",@"快餐",@"火锅",@"日韩料理",@"西餐",@"烧烤小吃"];
+    self.movices = @[@"内地剧",@"港台剧",@"英美剧"];
+    self.hostels = @[@"经济酒店",@"商务酒店",@"连锁酒店",@"度假酒店",@"公寓酒店"];
+    self.sorts = @[@"默认排序",@"离我最近",@"好评优先",@"人气优先",@"最新发布"];
+    
+    // 添加下拉菜单
+    DOPDropDownMenu *menu = [[DOPDropDownMenu alloc] initWithOrigin:CGPointMake(0, 64) andHeight:44];
+    menu.delegate = self;
+    menu.dataSource = self;
+    
+    [self.view addSubview:menu];
+    _menu = menu;
+    
+    // 创建menu 第一次显示 不会调用点击代理，可以用这个手动调用
+    [menu selectDefalutIndexPath];
+}
+
+- (void)menuReloadData
+{
+    self.classifys = @[@"美食",@"今日新单",@"电影"];
+    [_menu reloadData];
+}
+- (IBAction)selectIndexPathAction:(id)sender {
+    [_menu selectIndexPath:[DOPIndexPath indexPathWithCol:0 row:0 item:0 unit:2]];
+}
+
+- (NSInteger)numberOfColumnsInMenu:(DOPDropDownMenu *)menu
+{
+    return 3;
+}
+
+- (NSInteger)menu:(DOPDropDownMenu *)menu numberOfRowsInColumn:(NSInteger)column
+{
+    if (column == 0) {
+        return self.provinces.count;
+    }else if (column == 1){
+        return self.classifys.count;
+    }else {
+        return self.sorts.count;
+    }
+}
+
+- (NSString *)menu:(DOPDropDownMenu *)menu titleForRowAtIndexPath:(DOPIndexPath *)indexPath
+{
+    if (indexPath.column == 0) {
+        return self.provinces[indexPath.row];
+    } else if (indexPath.column == 1){
+        return self.classifys[indexPath.row];
+    } else {
+        return self.sorts[indexPath.row];
+    }
+}
+
+// new datasource
+
+- (NSString *)menu:(DOPDropDownMenu *)menu imageNameForRowAtIndexPath:(DOPIndexPath *)indexPath
+{
+    if (indexPath.column == 1) {
+        return [NSString stringWithFormat:@"ic_filter_category_%ld",indexPath.row];
+    }
+    return nil;
+}
+
+- (NSString *)menu:(DOPDropDownMenu *)menu imageNameForItemsInRowAtIndexPath:(DOPIndexPath *)indexPath
+{
+    if (indexPath.column == 1 && indexPath.row >= 0) {
+        return [NSString stringWithFormat:@"ic_filter_category_%ld",indexPath.item];
+    }
+    return nil;
+}
+
+// new datasource
+
+- (NSString *)menu:(DOPDropDownMenu *)menu detailTextForRowAtIndexPath:(DOPIndexPath *)indexPath
+{
+    if (indexPath.column == 1) {
+        return [@(arc4random()%1000) stringValue];
+    }
+    return nil;
+}
+
+- (NSString *)menu:(DOPDropDownMenu *)menu detailTextForItemsInRowAtIndexPath:(DOPIndexPath *)indexPath
+{
+    if (indexPath.column != 0) {
+        return [@(arc4random()%1000) stringValue];
+    }
+    return nil;
+}
+
+- (NSInteger)menu:(DOPDropDownMenu *)menu numberOfItemsInRow:(NSInteger)row column:(NSInteger)column
+{
+    if (column == 0) {
+        if (row == 0) {
+            return self.city1.count;
+        } else if (row == 1) {
+            return self.city2.count;
+        } else {
+            return 0;
+        }
+    } else if (column == 1) {
+        if (row == 0) {
+            return self.cates.count;
+        } else if (row == 2){
+            return self.movices.count;
+        } else if (row == 3){
+            return self.hostels.count;
+        }
+    }
+    return 0;
+}
+
+- (NSString *)menu:(DOPDropDownMenu *)menu titleForItemsInRowAtIndexPath:(DOPIndexPath *)indexPath
+{
+    if (indexPath.column == 0) {
+        if (indexPath.row == 0) {
+            return self.city1[indexPath.item];
+        } else if (indexPath.row == 1) {
+            return self.city2[indexPath.item];
+        } else {
+            return 0;
+        }
+    } else if (indexPath.column == 1) {
+        if (indexPath.row == 0) {
+            return self.cates[indexPath.item];
+        } else if (indexPath.row == 2){
+            return self.movices[indexPath.item];
+        } else if (indexPath.row == 3){
+            return self.hostels[indexPath.item];
+        }
+    }
+    return nil;
+}
+
+- (NSInteger)menu:(DOPDropDownMenu *)menu numberOfUnitsInItem:(NSInteger)item row:(NSInteger)row column:(NSInteger)column {
+    if (column == 0) {
+        if (row == 0) {
+            if (item == 0) {
+                return self.address1_1.count;
+            } else if (item == 1) {
+                return self.address1_2.count;
+            } else {
+                return self.address1_3.count;
+            }
+        } else {
+            return 0;
+        }
+    }
+    return 0;
+}
+
+- (NSString *)menu:(DOPDropDownMenu *)menu titleForUnitsInItemAtIndexPath:(DOPIndexPath *)indexPath {
+    if (indexPath.column == 0) {
+        if (indexPath.row == 0) {
+            if (indexPath.item == 0) {
+                return self.address1_1[indexPath.unit];
+            } else if (indexPath.item == 1) {
+                return self.address1_2[indexPath.unit];
+            } else {
+                return self.address1_3[indexPath.unit];
+            }
+        } else {
+            return nil;
+        }
+    }
+    return nil;
+}
+
+- (void)menu:(DOPDropDownMenu *)menu didSelectRowAtIndexPath:(DOPIndexPath *)indexPath
+{
+    if (indexPath.unit >= 0) {
+        NSLog(@"点击了 %ld - %ld - %ld - %ld 项目",indexPath.column,indexPath.row,indexPath.item, indexPath.unit);
+    } else {
+        if (indexPath.item >= 0) {
+            NSLog(@"点击了 %ld - %ld - %ld 项目",indexPath.column,indexPath.row,indexPath.item);
+        }else {
+            NSLog(@"点击了 %ld - %ld 项目",indexPath.column,indexPath.row);
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ README.md
@@ -0,0 +1,37 @@
+## DOPDropDownMenu-PK
+项目需求以及配合设计的完美方案，在[DOPDropDownMenu-Enhanced](https://github.com/12207480/DOPDropDownMenu-Enhanced)的基础上添加新功能，希望可以帮助到有类似需求的同学，如有任务问题请issuess我:  
+
+* 主要功能支持三级菜单
+* 支持选择后保持菜单展开
+* 优化三角指示图标颜色设置后收起菜单后复原的问题
+
+## 应用截图
+![image](https://github.com/peanutNote/DOPDropDownMenu-PK/blob/master/DOPDropDownMenu-PK/demo.gif)
+
+![image](https://github.com/peanutNote/DOPDropDownMenu-PK/blob/master/DOPDropDownMenu-PK/demo1.gif)
+
+![image](https://github.com/peanutNote/DOPDropDownMenu-PK/blob/master/DOPDropDownMenu-PK/demo2.gif)
+
+## 用法
+与DOPDropDownMenu-Enhanced用法基本一致主要添加一下内容  
+
+```objc
+#pragma mark QYNew
+/** 新增
+ *  当有column列 row 行 item项 返回有多少个单元，如果>0，说明有三级级列表 ，=0 没有三级列表
+ *  如果都没有可以不实现该协议
+ */
+- (NSInteger)menu:(DOPDropDownMenu *)menu numberOfUnitsInItem:(NSInteger)item row:(NSInteger)row column:(NSInteger)column;
+
+/** 新增
+ *  当有column列 row 行 item项 unit单元 title
+ *  如果都没有可以不实现该协议
+ */
+- (NSString *)menu:(DOPDropDownMenu *)menu titleForUnitsInItemAtIndexPath:(DOPIndexPath *)indexPath;
+
+#pragma mark property_new
+// 当有三级列表unit时，点击item 是否调用点击代理方法
+@property (nonatomic, assign) BOOL isClickHaveUnitValid;
+// 选择后是否让菜单收起
+@property (nonatomic, assign) BOOL isKeepDOPMenu;
+```
